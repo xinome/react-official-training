0b7744a4d75bbe5a264453a2dbefc789e33df40a
xinome <xinome2038@gmail.com>
2023-11-17 18:58:16 +0900

    課題解答

--- passing-props-to-a-component_test02/src/App.js
+++ passing-props-to-a-component_test02/src/App.js
@@ -1,10 +1,15 @@
 import { getImageUrl } from './utils.js';
 
 function Avatar({ person, size }) {
+  let thumbSize = 's';
+  if(size > 90) {
+    thumbSize = 'b';
+  }
+
   return (
     <img
       className="avatar"
-      src={getImageUrl(person, 'b')}
+      src={getImageUrl(person, thumbSize)}
       alt={person.name}
       width={size}
       height={size}
@@ -14,12 +19,22 @@ function Avatar({ person, size }) {
 
 export default function Profile() {
   return (
-    <Avatar
-      size={40}
-      person={{ 
-        name: 'Gregorio Y. Zara', 
-        imageId: '7vQD0fP'
-      }}
-    />
+    <>
+      <Avatar
+        size={40}
+        person={{ 
+          name: 'Gregorio Y. Zara', 
+          imageId: '7vQD0fP'
+        }}
+      />
+      <Avatar
+        size={120}
+        person={{ 
+          name: 'Gregorio Y. Zara', 
+          imageId: '7vQD0fP'
+        }}
+      />
+    </>
+    
   );
 }
