8cb82514028764c45fd16cf657fe0c3f475094bd
xinome <xinome2038@gmail.com>
2023-11-17 19:55:11 +0900

    課題解答

--- rendering-lists_test01/src/App.js
+++ rendering-lists_test01/src/App.js
@@ -2,23 +2,46 @@ import { people } from './data.js';
 import { getImageUrl } from './utils.js';
 
 export default function List() {
-  const listItems = people.map(person =>
-    <li key={person.id}>
-      <img
-        src={getImageUrl(person)}
-        alt={person.name}
-      />
-      <p>
-        <b>{person.name}:</b>
-        {' ' + person.profession + ' '}
-        known for {person.accomplishment}
-      </p>
-    </li>
-  );
+  // chemistとchemist以外の人を分ける
+  const chemists = people.filter(person => person.profession === 'chemist');
+  const others = people.filter(person => person.profession !== 'chemist');
+
   return (
     <article>
       <h1>Scientists</h1>
-      <ul>{listItems}</ul>
+      <h2>Chemists</h2>
+      <ul>
+        {chemists.map(person =>
+          <li key={person.id}>
+            <img
+              src={getImageUrl(person)}
+              alt={person.name}
+            />
+            <p>
+              <b>{person.name}:</b>
+              {' ' + person.profession + ' '}
+              known for {person.accomplishment}
+            </p>
+          </li>
+        )}
+      </ul>
+
+      <h2>Other Scientists</h2>
+      <ul>
+        {others.map(person =>
+          <li key={person.id}>
+            <img
+              src={getImageUrl(person)}
+              alt={person.name}
+            />
+            <p>
+              <b>{person.name}:</b>
+              {' ' + person.profession + ' '}
+              known for {person.accomplishment}
+            </p>
+          </li>
+        )}
+      </ul>
     </article>
   );
 }
