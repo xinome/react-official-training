954ef16f6f46f6e56fe9b71344ecc450025ce70c
xinome <xinome2038@gmail.com>
2023-11-17 17:56:19 +0900

    課題解答

--- writing-markup-with-jsx_test01/src/App.js
+++ writing-markup-with-jsx_test01/src/App.js
@@ -1,12 +1,26 @@
 export default function Bio() {
   return (
-    <div class="intro">
-      <h1>Welcome to my website!</h1>
-    </div>
-    <p class="summary">
-      You can find my thoughts here.
-      <br><br>
-      <b>And <i>pictures</b></i> of scientists!
-    </p>
+    <>
+      <div class="intro">
+        <h1>Welcome to my website!</h1>
+      </div>
+      <p class="summary">
+        You can find my thoughts here.
+        <br /><br />
+        <b>And <i>pictures</i></b> of scientists!
+      </p>
+    </>
   );
 }
+
+/*
+JSX のルール 
+1. 単一のルート要素を返す 
+コンポーネントから複数の要素を返すには、それを単一の親タグで囲みます。
+<div>や<>（<fragment>）など
+
+2. すべてのタグを閉じる 
+HTML と同様に、すべてのタグを閉じる必要があります。
+
+3. （ほぼ）すべてキャメルケースで！ 
+*/
