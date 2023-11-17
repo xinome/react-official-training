a6ac3555cb9f01684713603f54fabc9b09bd58fe
xinome <xinome2038@gmail.com>
2023-11-17 20:19:23 +0900

    課題解答

--- rendering-lists_test03/src/App.js
+++ rendering-lists_test03/src/App.js
@@ -1,21 +1,36 @@
 import { recipes } from './data.js';
 
+function Recipe({
+  id, name, ingredients
+}) {
+  return (
+    <div key={id}>
+      <h2>{name}</h2>
+      <ul>
+        {ingredients.map(ingredient =>
+          <li key={ingredient}>
+            {ingredient}
+          </li>
+        )}
+      </ul>
+    </div>
+  );
+}
+
 export default function RecipeList() {
   return (
     <div>
       <h1>Recipes</h1>
       {recipes.map(recipe =>
-        <div key={recipe.id}>
-          <h2>{recipe.name}</h2>
-          <ul>
-            {recipe.ingredients.map(ingredient =>
-              <li key={ingredient}>
-                {ingredient}
-              </li>
-            )}
-          </ul>
-        </div>
+        <Recipe
+          {...recipe}
+          key={recipe.id}
+        />
       )}
     </div>
   );
 }
+
+/*
+ここで <Recipe {...recipe} key={recipe.id} /> というのは「recipe オブジェクトのすべてのプロパティを Recipe コンポーネントの props として渡せ」という意味のショートカット構文です。
+*/
