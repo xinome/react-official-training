0aa432c84a0df13076d6d3cb767745e2f6134cbd
xinome <xinome2038@gmail.com>
2023-11-17 20:55:46 +0900

    keeping-components-pure_test02 セットアップ

---
+++ keeping-components-pure_test02/src/App.js
@@ -0,0 +1,16 @@
+import Profile from './Profile.js';
+
+export default function App() {
+  return (
+    <>
+      <Profile person={{
+        imageId: 'lrWQx8l',
+        name: 'Subrahmanyan Chandrasekhar',
+      }} />
+      <Profile person={{
+        imageId: 'MK3eW3A',
+        name: 'Creola Katherine Johnson',
+      }} />
+    </>
+  )
+}
