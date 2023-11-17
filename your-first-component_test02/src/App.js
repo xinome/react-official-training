f580a91a59deb9e6a34a9f93fe47afbef1a585a1
xinome <xinome2038@gmail.com>
2023-11-17 15:08:30 +0900

    課題回答

--- your-first-component_test02/src/App.js
+++ your-first-component_test02/src/App.js
@@ -1,4 +1,5 @@
 export default function Profile() {
-  return
-    <img src="https://i.imgur.com/jA8hHMpm.jpg" alt="Katsuko Saruhashi" />;
+  return (
+    <img src="https://i.imgur.com/jA8hHMpm.jpg" alt="Katsuko Saruhashi" />
+  );
 }
