142dea1a1486c4ec286061f7aab721386bbbeb1c
xinome <xinome2038@gmail.com>
2023-11-17 19:40:56 +0900

    課題解答

--- conditional-rendering_test03/src/App.js
+++ conditional-rendering_test03/src/App.js
@@ -1,14 +1,25 @@
 function Drink({ name }) {
+  let plant, caffeine, age;
+  if (name === 'tea') {
+    plant = 'leaf';
+    caffeine = '15–70 mg/cup';
+    age = '4,000+ years';
+  } else if (name === 'coffee') {
+    plant = 'bean';
+    caffeine = '80–185 mg/cup';
+    age = '1,000+ years';
+  }
+
   return (
     <section>
       <h1>{name}</h1>
       <dl>
         <dt>Part of plant</dt>
-        <dd>{name === 'tea' ? 'leaf' : 'bean'}</dd>
+        <dd>{plant}</dd>
         <dt>Caffeine content</dt>
-        <dd>{name === 'tea' ? '15–70 mg/cup' : '80–185 mg/cup'}</dd>
+        <dd>{caffeine}</dd>
         <dt>Age</dt>
-        <dd>{name === 'tea' ? '4,000+ years' : '1,000+ years'}</dd>
+        <dd>{age}</dd>
       </dl>
     </section>
   );
