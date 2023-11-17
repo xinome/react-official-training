2605cda7976b7eb9060177534b0b15fea7e88adb
xinome <xinome2038@gmail.com>
2023-11-17 15:56:08 +0900

    課題回答

--- your-first-component_test04/src/App.js
+++ your-first-component_test04/src/App.js
@@ -1,2 +1,14 @@
 // Write your component below!
 
+export default function App() {
+  return (
+    <>
+      <section>
+        <h1>Amazing scientists</h1>
+      </section>
+      <section>
+        <h1>Amazing React</h1>
+      </section>
+    </>
+  );
+}
