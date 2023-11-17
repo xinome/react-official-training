56d16db776e6e681ed2a75737f67508971840f6d
xinome <xinome2038@gmail.com>
2023-11-17 21:02:13 +0900

    keeping-components-pure_test03 セットアップ

---
+++ keeping-components-pure_test03/src/StoryTray.js
@@ -0,0 +1,16 @@
+export default function StoryTray({ stories }) {
+  stories.push({
+    id: 'create',
+    label: 'Create Story'
+  });
+
+  return (
+    <ul>
+      {stories.map(story => (
+        <li key={story.id}>
+          {story.label}
+        </li>
+      ))}
+    </ul>
+  );
+}
