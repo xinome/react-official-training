8356d40a1219174b5e53f8c299f150c6f0cb1499
xinome <xinome2038@gmail.com>
2023-11-17 18:25:07 +0900

    課題解答

--- javascript-in-jsx-with-curly-braces_test03/src/App.js
+++ javascript-in-jsx-with-curly-braces_test03/src/App.js
@@ -16,7 +16,7 @@ export default function TodoList() {
       <h1>{person.name}'s Todos</h1>
       <img
         className="avatar"
-        src="{baseUrl}{person.imageId}{person.imageSize}.jpg"
+        src={baseUrl + person.imageId + person.imageSize + ".jpg"}
         alt={person.name}
       />
       <ul>
