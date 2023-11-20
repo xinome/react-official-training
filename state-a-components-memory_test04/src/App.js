940e877d28e9116fc06fa105783a34d79d294071
xinome <xinome2038@gmail.com>
2023-11-20 20:03:02 +0900

    課題解答

--- state-a-components-memory_test04/src/App.js
+++ state-a-components-memory_test04/src/App.js
@@ -1,10 +1,9 @@
 import { useState } from 'react';
 
 export default function FeedbackForm() {
-  const [name, setName] = useState('');
 
   function handleClick() {
-    setName(prompt('What is your name?'));
+    const name = prompt('What is your name?');
     alert(`Hello, ${name}!`);
   }
 
@@ -14,3 +13,10 @@ export default function FeedbackForm() {
     </button>
   );
 }
+
+/*
+state は、コンポーネントのレンダー結果に影響するデータです。
+state を使わないデータは、state に含める必要はありません。
+例えば、ユーザーが入力した値を state に含める必要はありません。
+代わりに、ユーザーが入力した値は、イベントハンドラーの中でローカル変数に保存してください。
+*/
