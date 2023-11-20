cae6060910ef0ed89086f506879329ce0f435051
xinome <xinome2038@gmail.com>
2023-11-20 19:59:42 +0900

    state-a-components-memory_test04 初回セットアップ

---
+++ state-a-components-memory_test04/src/App.js
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+
+export default function FeedbackForm() {
+  const [name, setName] = useState('');
+
+  function handleClick() {
+    setName(prompt('What is your name?'));
+    alert(`Hello, ${name}!`);
+  }
+
+  return (
+    <button onClick={handleClick}>
+      Greet
+    </button>
+  );
+}
